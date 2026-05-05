37016ef635cf5fce56b9d815c4217ef3c785599f
Joshua Dahl <joshuadahl@unr.edu>
2026-05-05 16:22:11 -0700

    Audio

--- Assets/Scripts/SetSceneAudio.cs
+++ Assets/Scripts/SetSceneAudio.cs
@@ -4,7 +4,7 @@ public class SetSceneAudio : MonoBehaviour {
     public AudioManagerBase.NamedAudioClip[] musicClips;
     public float fadeDuration = 3;
     
-    public void Start() {
+    public void OnEnable() {
         var audioManager = FindObjectsByType<AudioManager>(FindObjectsInactive.Include, FindObjectsSortMode.None)[0];
         audioManager.musicPlayer.ClearTracks();
         audioManager.musicPlayer.AddTracks(musicClips);
